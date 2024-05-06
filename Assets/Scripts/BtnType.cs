80c6a5d2180e6eba1f01e961c3f4f573f1056ce3
vnlfltmxm <163379543+vnlfltmxm@users.noreply.github.com>
2024-05-06 13:47:36 +0900

    Revert "Merge pull request #20 from Berry6753/choigg"
    This reverts commit 8ebc0e9ba9752f0b9a29482f9223b3f677e77ffe, reversing
    changes made to 00e27373fdcd88393893f34f13ebe74ab297c28c.

--- Assets/Scripts/BtnType.cs
+++ Assets/Scripts/BtnType.cs
@@ -1,45 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 
-public class BtnType : MonoBehaviour, IPointerEnterHandler ,IPointerExitHandler
+public class BtnType : MonoBehaviour
 {
     public BTNType currentType;
-    public Transform buttonScale;
-    Vector3 defaultScale;
-
-    private void Start()
-    {
-        defaultScale = buttonScale.localScale;
-    }
     public void OnBtnClick()
     {
         switch (currentType)
         {
             case BTNType.New:
-            SceneManager.LoadScene("Loading");
+                Debug.Log("새게임");
                 break;
             case BTNType.Continue:
                 Debug.Log("이어하기");
                 break;
-            case BTNType.Option:
-                Debug.Log("옵션");
-                break;
-            case BTNType.Quit:
-                break;
-               
         }
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        buttonScale.localScale = defaultScale*1.2f;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        buttonScale.localScale = defaultScale;
     }
 }
